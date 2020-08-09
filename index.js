c5b0ce6683e1bfc659963895552e879c8916c308
pbedrosian <peter_bedrosian@Yahoo.com>
2020-08-09 11:23:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function writeCards(names, holiday) {
+    const array = []
     for (let i = 0; i < names.length; i++) {
-      console.log(`Thank you ${names[i]}, for the wonderful ${holiday} gift!`);
+      let card = (`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+      array.push(card)
     }
+    return array
+};
+
+function countDown(number) {
+    let starting_number = number
+    while (starting_number >= 0) { console.log(starting_number--) }
 };
 
